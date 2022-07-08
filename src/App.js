bc04368d99e53887fe370fd6479ece1d5ce52464
Jason Hawgood <jason@seavistasoftware.com>
2022-07-08 16:21:57 -0400

    Test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Coming soon...
         </a>
       </header>
     </div>
