5232b17f91bea26b3619c4b558f7588d5a24d81f
Jason Hawgood <jason@seavistasoftware.com>
2022-07-08 16:25:01 -0400

    Fixes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Coming soon...
+          Coming Soon...
         </a>
       </header>
     </div>
