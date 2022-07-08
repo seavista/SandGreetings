325572b2ee5e1ce99b46f2157c1ee5086290d136
Jason Hawgood <jason@seavistasoftware.com>
2022-07-08 16:28:10 -0400

    Title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Coming Soon...
+          Sand Greetings
         </a>
       </header>
     </div>
