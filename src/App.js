3a7899bdccc4615461ca3bb1c1a9b498918d798c
Jason Hawgood <jason@seavistasoftware.com>
2022-07-08 21:16:16 -0400

    Alpha Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react';
+import { useLocation, Routes, Route, BrowserRouter } from 'react-router-dom';
+import AppRoute from './utils/AppRoute';
+import ScrollReveal from './utils/ScrollReveal';
+import ReactGA from 'react-ga';
 
-function App() {
+
+
+// Layouts
+import LayoutDefault from './layouts/LayoutDefault';
+
+// Views 
+import Home from './views/Home';
+
+// Initialize Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+
+const trackPage = page => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
+
+const App = () => {
+
+  const childRef = useRef();
+  let location = useLocation();
+
+  useEffect(() => {
+    const page = location.pathname;
+    document.body.classList.add('is-loaded');
+    document.title ="Sand Greetings | Say it in Sand"
+    childRef.current.init();
+    trackPage(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sand Greetings
-        </a>
-      </header>
-    </div>
+  
+    <ScrollReveal
+      ref={childRef}
+      children={() => (
+        <Routes>
+          <Route path="/" element={<LayoutDefault><Home /></LayoutDefault>} />
+        </Routes>
+      
+      )} />
+
   );
 }
 
